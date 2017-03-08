f1f7aa9078afcc5bc5ee9c95ffca22fc9f64fab1
LDalby <lars@bios.au.dk>
2017-03-08 23:02:26 +0100

    Adapt the polyref script to the subset map

--- MakingThePolyrefNorway.R
+++ MakingThePolyrefNorway.R
@@ -15,28 +15,29 @@ GetNR = function(x){
 }
 
 staticpath = 'o:/ST_LandskabsGenerering/Norway/NTrondelag/Landscape/outputs'
-attr = fread(file.path(staticpath, 'Attr_NTrondelag.csv'))
+attr = fread(file.path(staticpath, paste0('Attr_NTrondelagSubset', '.csv')))
 setkey(attr, 'LINK')
-recl = fread(file.path(staticpath, 'Reclass_Completemap_NTrondelag.txt'), sep = ':')
+recl = fread(file.path(staticpath, paste0('Reclass_Completemap_Subset','.txt')), sep = ':')
 setnames(recl, c('PolyType', 'LINK'))
 setkey(recl, 'LINK')
 
 tmp = merge(attr, recl)  # Okay fine, now we can get rid of LINK.
 tmp[, LINK:=NULL]
-farmlink = fread(file.path(staticpath, 'FarmLinkTable_NTrondelag.txt'))
+farmlink = fread(file.path(staticpath, paste0('FarmLinkTable_NTrondelagSubset', '.txt')))
 farmlink = farmlink[PolyType >= 2100000,]
 farmlink[,FarmNumber:=sapply(FarmID, FUN = GetNR)]
-# farmlink[is.na(FarmNumber),]
+farmlink[is.na(FarmNumber),FarmNumber:=-9999L]  # Any fields without owner gets this NA indicator.
 setkey(farmlink, PolyType)
 setkey(tmp, PolyType)
 
 full = merge(tmp, farmlink, all.x = TRUE)
 full[is.na(FarmNumber), FarmNumber:=-1L]
+full[, FarmID:=NULL]
 
 full[PolyType %between% c(2100000, 2299999), PolyType:=20]  # Field
 full[PolyType %between% c(2300000, 2399999), PolyType:=35]  # Permnanent pasture
 setkey(full, PolyRef)
-fwrite(full, file = file.path(staticpath, 'ALMaSSTrondelag','PolyrefTestNTrondelag.txt'), sep = '\t')
+fwrite(full, file = file.path(staticpath, 'ALMaSSTrondelagSubset','PolyrefNTrondelagSubset.txt'), sep = '\t')
 
 tables()
 
